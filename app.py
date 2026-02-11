b4a3cb459779b07986f9b25bcc46a954db471c1b
Shishir Adhikari <shishiradhikari2058@gmail.com>
2026-02-10 21:17:26 -0600

    Update landing page / routes

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-import eventlet
-eventlet.monkey_patch()
-
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret-key"
 
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
-    async_mode="eventlet"
-)
-
-# ---------- ROUTES ----------
 @app.route("/")
-def home():
-    return "✅ CheckMyPlatform backend will be LIVE SOON"
+def index():
+    return render_template("index.html")
 
 @app.route("/player")
 def player():
@@ -26,32 +14,5 @@ def player():
 def agent():
     return render_template("agent.html")
 
-# ---------- SOCKET EVENTS ----------
-@socketio.on("connect")
-def connect():
-    print("Client connected")
-
-@socketio.on("join")
-def join(data):
-    room = data["room"]   # room = player_id
-    join_room(room)
-    print(f"Joined room: {room}")
-
-@socketio.on("send_message")
-def send_message(data):
-    emit(
-        "receive_message",
-        {
-            "sender": data["sender"],   # "player" or "agent"
-            "message": data["message"]
-        },
-        room=data["room"]
-    )
-
-@socketio.on("disconnect")
-def disconnect():
-    print("Client disconnected")
-
-# ---------- START ----------
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    app.run()
